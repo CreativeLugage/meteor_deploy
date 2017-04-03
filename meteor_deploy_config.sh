c376e0f041303cc7f792c2c7568df14b8108938b
Andrei Costin Balaianu <contact@balaianu.ro>
2017-04-03 17:07:14 +0300

    Update meteor_deploy_config.sh

--- meteor_deploy_config.sh
+++ meteor_deploy_config.sh
@@ -6,16 +6,16 @@
 
 # Deployment config for: My App
 
-export METEOR_DEPLOY_APP_SOURCE_FOLDER="/fuul/path/to/source"
-export METEOR_DEPLOY_APP_DESTINATION_FOLDER="/full/path/to/destination"
-export METEOR_DEPLOY_SERVER_NAME="https://live.servername.com"
+export METEOR_DEPLOY_APP_SOURCE_FOLDER="/full/path/to/source"   # local machine
+export METEOR_DEPLOY_APP_DESTINATION_FOLDER="/full/path/to/destination"   # local machine
+export METEOR_DEPLOY_SERVER_NAME="https://live.servername.com"  # URL for the deployed application
 
-export METEOR_DEPLOY_APP_BUNDLE_FILE_NAME="bundled_app.tar.gz"
+export METEOR_DEPLOY_APP_BUNDLE_FILE_NAME="bundled_app.tar.gz"    # file name for the .tar.gs bundle
 
 export METEOR_DEPLOY_APP_NAME="my-app-name"
 export METEOR_DEPLOY_KEYSTORE_FILE="my-app-name.keystore"
 export METEOR_DEPLOY_KEYSTORE_PASSWORD="MyKeystorePassword"
 
-export METEOR_DEPLOY_SERVER_ADDRESS="xx.xx.xx.xx"
-export METEOR_DEPLOY_SERVER_PASSWORD="MyRemoteServerPassword"
-export METEOR_DEPLOY_SERVER_DESTINATION_FOLDER="/full/remote/server/destination"
+export METEOR_DEPLOY_SERVER_ADDRESS="xx.xx.xx.xx"   # address to access via SSH (remote server)
+export METEOR_DEPLOY_SERVER_PASSWORD="MyRemoteServerPassword"   # remote server SSH password
+export METEOR_DEPLOY_SERVER_DESTINATION_FOLDER="/full/remote/server/destination"    # Destination folder path on the REMOTE server
