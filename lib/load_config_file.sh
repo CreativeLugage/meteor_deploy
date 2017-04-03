ad4ab6402613a0cd2699f01292dafb12f43d5190
Andrei Balaianu <andrei.balaianu@creativeluggage.com>
2017-04-03 15:24:24 +0300

    Use config files in the 'configs' folder for convenience; fall back to manual file input or skip config file

--- lib/load_config_file.sh
+++ lib/load_config_file.sh
@@ -5,9 +5,57 @@ load_config_file()
 	case $input_config in
 	    [yY][eE][sS]|[yY])
 			
-			echo "Input full path to .sh configuration file (and press Enter): "
-			read BASH_CONFIG_FILE
-			source ${BASH_CONFIG_FILE}
+			# echo "Input full path to .sh configuration file (and press Enter): "
+			# read BASH_CONFIG_FILE
+			# source ${BASH_CONFIG_FILE}
+
+			## Collect the files in the array $files
+			files=( ${SCRIPTPATH}/configs/*.sh )
+			## Enable extended globbing. This lets us use @(foo|bar) to
+			## match either 'foo' or 'bar'.
+			shopt -s extglob
+
+			## Start building the string to match against.
+			string="@(${files[0]}"
+			## Add the rest of the files to the string
+			for((i=1;i<${#files[@]};i++))
+			do
+			    string+="|${files[$i]}"
+			done
+			## Close the parenthesis. $string is now @(file1|file2|...|fileN)
+			string+=
+			string+=")"
+
+			## Show the menu. This will list all files and the string "quit"
+			select file in "${files[@]}" "manual" "skip"
+			do
+			    case $file in
+				    ## If the choice is one of the files (if it matches $string)
+				    $string)
+				        source ${file}
+				        break
+			        ;;
+
+				    "manual")
+				        echo "Input full path to .sh configuration file (and press Enter): "
+				        read BASH_CONFIG_FILE
+				        source ${BASH_CONFIG_FILE}
+				        break
+				    ;;
+
+				    "skip")
+				        echo "Skipping configuration file! Add the required parameters manually!"
+						echo ""
+						break
+					;;
+				    
+				    *)
+				        file=""
+				        echo "Please choose a number from 1 to $((${#files[@]}+1))"
+			        ;;
+
+			    esac
+			done
 
 		;;
 
